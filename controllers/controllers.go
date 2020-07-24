8a781ac4e61f8b53e82192d4476128fd9a5bd46a
fabstao <fsalaman@gmail.com>
2020-07-24 16:49:33 -0500

    Improved security, refactoring

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -7,33 +7,20 @@ import (
 	"time"
 
 	"github.com/labstack/echo"
-	"gitlab.com/fabstao/fabsgoblog/models"
 	"gitlab.com/fabstao/fabsgoblog/views"
-	"golang.org/x/crypto/bcrypt"
 )
 
-var cookie http.Cookie
+//var cookie http.Cookie
 
-// Login Handler
-func Login(c echo.Context) error {
-	datos := struct {
-		Title        string
-		Header       string
-		Footer       string
-		MensajeFlash string
-	}{
-		Title:        views.Comunes.Title,
-		Header:       views.Comunes.Header,
-		Footer:       views.Comunes.Footer,
-		MensajeFlash: "",
-	}
-	return c.Render(http.StatusOK, "login.html", datos)
+// PageRender struct helper for security rendering
+type PageRender struct {
+	Username     string
+	Title        string
+	Header       string
+	Footer       string
+	MensajeFlash string
+	Alerta       string
 }
-
-// Signup : Registrarse
-//func Signup(c echo.Context) error {
-
-//}
 
 // Inicio Handler
 func Inicio(c echo.Context) error {
@@ -65,107 +52,4 @@ func Hello(c echo.Context) error {
 	content.Timestamp = time.Now().UTC()
 	content.Random = rand.Intn(1000)
 	return c.JSON(http.StatusOK, content)
-}
-
-// Nuevo - controlador para formulario nuevo usuario
-func Nuevo(c echo.Context) error {
-	datos := struct {
-		Username     string
-		Title        string
-		Header       string
-		Footer       string
-		MensajeFlash string
-	}{
-		Username:     "",
-		Title:        views.Comunes.Title,
-		Header:       views.Comunes.Header,
-		Footer:       views.Comunes.Footer,
-		MensajeFlash: "",
-	}
-	return c.Render(http.StatusOK, "crearusuario.html", datos)
-}
-
-// Crear usuario
-func Crear(c echo.Context) error {
-	datos := struct {
-		Username     string
-		Title        string
-		Header       string
-		Footer       string
-		MensajeFlash string
-		Alerta       string
-	}{
-		Username:     "",
-		Title:        views.Comunes.Title,
-		Header:       views.Comunes.Header,
-		Footer:       views.Comunes.Footer,
-		MensajeFlash: "",
-		Alerta:       "success",
-	}
-	var usuario models.User
-	var checausuario models.User
-	var rol models.Role
-	user := c.FormValue("usuario")
-	email := c.FormValue("email")
-	models.Dbcon.Where("username = ?", user).Find(&checausuario)
-	if len(checausuario.Email) > 0 {
-		datos.MensajeFlash = "Usuario ya existe: " + user
-		datos.Alerta = "danger"
-		return c.Render(http.StatusOK, "crearusuario.html", datos)
-	}
-	models.Dbcon.Where("email = ?", email).Find(&checausuario)
-	if len(checausuario.Email) > 0 {
-		datos.MensajeFlash = "Dirección de correo-e ya existe: " + email
-		datos.Alerta = "danger"
-		return c.Render(http.StatusOK, "crearusuario.html", datos)
-	}
-	models.Dbcon.Where("role = ?", "usuario").Find(&rol)
-	usuario.Username = user
-	usuario.Email = email
-	hashed, _ := bcrypt.GenerateFromPassword([]byte(c.FormValue("password")), bcrypt.DefaultCost)
-	usuario.Password = string(hashed)
-	usuario.Role = rol
-
-	fmt.Println(usuario)
-	models.Dbcon.Create(&usuario)
-	datos.MensajeFlash = "Usuario " + usuario.Username + " creado exitosamente"
-	return c.Render(http.StatusOK, "crearusuario.html", datos)
-}
-
-// Checklogin POST verificar login
-func Checklogin(c echo.Context) error {
-	var usuario models.User
-	email := c.FormValue("email")
-	password := c.FormValue("password")
-	fmt.Print("Login: ", email)
-	models.Dbcon.Where("email = ?", email).Find(&usuario)
-	fmt.Print("Login: Successful")
-	datos := struct {
-		Title        string
-		Header       string
-		Footer       string
-		MensajeFlash string
-	}{
-		Title:        views.Comunes.Title,
-		Header:       views.Comunes.Header,
-		Footer:       views.Comunes.Footer,
-		MensajeFlash: "",
-	}
-	fmt.Println(datos)
-	if len(usuario.Email) < 1 {
-		datos.MensajeFlash = "Correo-e o contraseña incorrectos"
-		return c.Render(http.StatusOK, "login.html", datos)
-	}
-	if err := bcrypt.CompareHashAndPassword([]byte(usuario.Password), []byte(password)); err != nil {
-		datos.MensajeFlash = "Correo-e o contraseña incorrectos"
-		return c.Render(http.StatusOK, "login.html", datos)
-	}
-
-	cookie.Name = "jsessionid"
-	cookie.Value = usuario.Email + "," + c.RealIP() + "," + usuario.Username
-	cookie.Expires = time.Now().Add(5 * time.Minute)
-	cookie.Domain = "localhost"
-	c.SetCookie(&cookie)
-	fmt.Println(usuario)
-	return c.Render(http.StatusOK, "index.html", datos)
 }
---
+++ controllers/security.go
@@ -0,0 +1,115 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo"
+	"gitlab.com/fabstao/fabsgoblog/models"
+	"gitlab.com/fabstao/fabsgoblog/views"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var cookie http.Cookie
+
+// Login Handler
+func Login(c echo.Context) error {
+	datos := PageRender{
+		Title:        views.Comunes.Title,
+		Header:       views.Comunes.Header,
+		Footer:       views.Comunes.Footer,
+		MensajeFlash: "",
+	}
+	return c.Render(http.StatusOK, "login.html", datos)
+}
+
+// Nuevo - controlador para formulario nuevo usuario
+func Nuevo(c echo.Context) error {
+	datos := PageRender{
+		Username:     "",
+		Title:        views.Comunes.Title,
+		Header:       views.Comunes.Header,
+		Footer:       views.Comunes.Footer,
+		MensajeFlash: "",
+	}
+	return c.Render(http.StatusOK, "crearusuario.html", datos)
+}
+
+// Crear usuario
+func Crear(c echo.Context) error {
+	datos := PageRender{
+		Username:     "",
+		Title:        views.Comunes.Title,
+		Header:       views.Comunes.Header,
+		Footer:       views.Comunes.Footer,
+		MensajeFlash: "",
+		Alerta:       "success",
+	}
+	var usuario models.User
+	var checausuario models.User
+	var rol models.Role
+	user := c.FormValue("usuario")
+	email := c.FormValue("email")
+	if len(user) < 4 || len(email) < 4 {
+		datos.MensajeFlash = "Usuario y correo no pueden estar vacíos o demasiado cortos"
+		datos.Alerta = "danger"
+		return c.Render(http.StatusOK, "crearusuario.html", datos)
+	}
+	models.Dbcon.Where("username = ?", user).Find(&checausuario)
+	if len(checausuario.Email) > 0 {
+		datos.MensajeFlash = "Usuario ya existe: " + user
+		datos.Alerta = "danger"
+		return c.Render(http.StatusOK, "crearusuario.html", datos)
+	}
+	models.Dbcon.Where("email = ?", email).Find(&checausuario)
+	if len(checausuario.Email) > 0 {
+		datos.MensajeFlash = "Dirección de correo-e ya existe: " + email
+		datos.Alerta = "danger"
+		return c.Render(http.StatusOK, "crearusuario.html", datos)
+	}
+	models.Dbcon.Where("role = ?", "usuario").Find(&rol)
+	usuario.Username = user
+	usuario.Email = email
+	hashed, _ := bcrypt.GenerateFromPassword([]byte(c.FormValue("password")), bcrypt.DefaultCost)
+	usuario.Password = string(hashed)
+	usuario.Role = rol
+
+	fmt.Println(usuario)
+	models.Dbcon.Create(&usuario)
+	datos.MensajeFlash = "Usuario " + usuario.Username + " creado exitosamente"
+	return c.Render(http.StatusOK, "crearusuario.html", datos)
+}
+
+// Checklogin POST verificar login
+func Checklogin(c echo.Context) error {
+	var usuario models.User
+	email := c.FormValue("email")
+	password := c.FormValue("password")
+	fmt.Print("Login: ", email)
+	models.Dbcon.Where("email = ?", email).Find(&usuario)
+	fmt.Print("Login: Successful")
+	datos := PageRender{
+		Title:        views.Comunes.Title,
+		Header:       views.Comunes.Header,
+		Footer:       views.Comunes.Footer,
+		MensajeFlash: "",
+	}
+	fmt.Println(datos)
+	if len(usuario.Email) < 1 {
+		datos.MensajeFlash = "Correo-e o contraseña incorrectos"
+		return c.Render(http.StatusOK, "login.html", datos)
+	}
+	if err := bcrypt.CompareHashAndPassword([]byte(usuario.Password), []byte(password)); err != nil {
+		datos.MensajeFlash = "Correo-e o contraseña incorrectos"
+		return c.Render(http.StatusOK, "login.html", datos)
+	}
+
+	cookie.Name = "jsessionid"
+	cookie.Value = usuario.Email + "," + c.RealIP() + "," + usuario.Username
+	cookie.Expires = time.Now().Add(5 * time.Minute)
+	cookie.Domain = "localhost"
+	c.SetCookie(&cookie)
+	fmt.Println(usuario)
+	return c.Render(http.StatusOK, "index.html", datos)
+}
