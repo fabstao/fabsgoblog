c2826f9a853986f9354c7e5b2184aab2a68d3869
fabstao <fsalaman@gmail.com>
2020-08-11 11:48:47 -0500

    Updated scripts

--- testLogin.sh
+++ testLogin.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+# *************************************************
+# (C) Fabs 2020
+# 
+# Args:
+# $1 - email
+# $2 - password
+# $3 - url base host (i.e. http://localhost:8019 )
+#
+# *************************************************
 
-curl -v -H "Content-Type: application/json" -X POST -d '{ "email": "fsalaman@gmail.com", "password": "password" }' http://localhost:8019/api/login
+if [ "$#" -le "3" ]; then                                                                                                              
+       echo "Usage: $0 <email> <password> <url basehost (i.e. http://localhost:8019)>"
+       exit 1                                                                                                                          
+fi      
+
+curl -v -H "Content-Type: application/json" -X POST -d '{ "email": "$1", "password": "$2" }' $3/api/login
--- testToken.sh
+++ testToken.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+# *************************************************
+# (C) Fabs 2020
+# 
+# Args:
+# $1 - Token
+# $2 - url base host (i.e. http://localhost:8019 )
+#
+# *************************************************
+
+if [ "$#" -le "3" ]; then                                                                                                              
+       echo "Usage: $0 <token> <url basehost (i.e. http://localhost:8019)>"
+       exit 1                                                                                                                          
+fi      
 
 curl -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer $1" \
- -d '{"titulo": "Prueba API", "texto":"![](https://i.imgur.com/FwLPvQa.png) # Es una prueba API \n ## REST \n * JSON \n * REST"}' \
- localhost:8019/sapi/ 
+ -d '{"titulo": "Prueba API", "texto":"![](https://i.imgur.com/FwLPvQa.png) \n # Es una prueba API \n ## REST \n * Esta entrada fue creada usando cURL \n * JSON \n * REST"}' \
+ $2/sapi/ 
